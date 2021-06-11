57ebdaeb4263b1b64590c6f9e14f833557174b64
Xuhao Luo <rossihox@gmail.com>
2021-06-11 03:43:15 +0000

    sample rate

--- scripts/client_metrics.py
+++ scripts/client_metrics.py
@@ -51,7 +51,7 @@ def get_metrics(dirname):
         commit_lats = []
         i = 0
         for l in f:
-            if i % 10000 == 0:
+            if i % 10 == 0:
                 l = l.split(' ')
                 try:
                     exec_lats.append(float(l[1]))
